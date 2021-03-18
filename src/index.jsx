4cd8cc04736a15975814eca7384c91379dabd7f2
Nikolaj Baer <nikolaj.baer@gmail.com>
2021-03-18 08:42:12 -0700

    move style to separate css file

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM, { render } from "react-dom";
 import { GameUI } from "./ui_components/GameUI"
+import "./style.css"
 
 ReactDOM.render( <GameUI />, document.getElementById("app"))
 
--- webpack.config.js
+++ webpack.config.js
@@ -27,25 +27,6 @@ module.exports = {
             }]
         },
         {
-          test: /\.(scss)$/,
-          use: [{
-            loader: 'style-loader', // inject CSS to page
-          }, {
-            loader: 'css-loader', // translates CSS into CommonJS modules
-          }, {
-            loader: 'postcss-loader', // Run postcss actions
-            options: {
-              plugins: function () { // postcss plugins, can be exported to postcss.config.js
-                return [
-                  require('autoprefixer')
-                ];
-              }
-            }
-          }, {
-            loader: 'sass-loader' // compiles Sass to CSS
-          }]
-        },
-        {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: ['babel-loader']
