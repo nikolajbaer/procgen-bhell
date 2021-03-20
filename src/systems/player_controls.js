de2b59dccb3c163310526448e445d6f312cf4a82
Nikolaj Baer <nikolaj.baer@gmail.com>
2021-03-20 10:20:58 -0700

    remove attempt at touch controls

--- src/systems/player_controls.js
+++ src/systems/player_controls.js
@@ -37,115 +37,31 @@ export class PlayerControlsSystem extends System {
                 event.preventDefault(); 
                 return false; 
             })
-            window.addEventListener("mouseup", event => { 
+            // TODO unbind this event listener on stop
+            this.mouseup_listener = (event) => { 
                 actions["Mouse"+event.button] = false; 
                 event.preventDefault(); 
                 return false; 
-            })
+            }
+            window.addEventListener("mouseup", this.mouseup_listener)
             // don't show the context menu (use RMB for game controls)
             render.addEventListener("contextmenu", event => {
                 event.preventDefault()
                 return false
             })
         }else{
-            // Touch Events
-            this.dpad_touch = null
-            this.aim_touch = null
-
-            // track two rects in the lower left/right
-            // where we do dpad and aim controls for touch
-            this.update_touch_rects()
-            window.addEventListener('resize', () => {
-                this.update_touch_rects()
-            })
-
-            render.addEventListener("touchstart", event => {
-                // maybe both touches are simultaneous?
-                Object.values(event.touches).forEach( t => {
-                    if(t.clientX < this.dpad_rect.x && t.clientY > this.dpad_rect.y){
-                        if(this.dpad_touch == null){
-                            console.log("DPAD touch started with ",t.identifier)
-                            this.dpad_touch = t.identifier
-                            this.update_touch_dpad(event.touches[0])
-                        }
-                    }else if(t.clientX > this.aim_rect.x && t.clientY > this.aim_rect.y){
-                        if(this.aim_touch == null){
-                            console.log("Aim Touch started with ",t.identifier)
-                            this.aim_touch = t.identifier
-                            actions["Mouse0"] = true
-                            this.update_touch_aim(event.touches[0])
-                        }
-                    }
-                })
-                event.preventDefault()
-                return false
-            })
-            render.addEventListener("touchmove", event => {
-                if(event.touches[this.dpad_touch]){
-                    this.update_touch_dpad(event.touches[this.dpad_touch])
-                }
-                if(event.touches[this.aim_touch]){
-                    this.update_touch_aim(event.touches[this.aim_touch])
-                }
-                event.preventDefault()
-                return false
-            })
-            const handleTouchEnd = (event) => {
-                if(event.changedTouches[this.dpad_touch]){
-                    console.log("clearing dpad touch",this.dpad_touch)
-                    this.dpad_touch = null
-                    this.direction = {x:0,y:0}
-                }
-                if(event.changedTouches[this.aim_touch]){
-                    console.log("clearing aim touch",this.aim_touch)
-                    this.aim_touch = null 
-                    actions["Mouse0"] = false
-                }
-                event.preventDefault()
-                return false
-            }
-            window.addEventListener("touchend",handleTouchEnd)
-            window.addEventListener("touchcancel",handleTouchEnd)
+            // TODO rethink touch controls
         } 
         this.direction = direction
         this.actions = actions
         this.mouse = mouse
     }
 
-    update_touch_rects(){
-        // relative touch control surfaces: e.g. 20% left corner and 20% right corner
-        const inv = 1 - TOUCH_PERCENTAGE 
-        const w = window.innerWidth*TOUCH_PERCENTAGE
-        const h = window.innerHeight * TOUCH_PERCENTAGE 
-        this.dpad_rect = {
-            x: window.innerWidth*TOUCH_PERCENTAGE,
-            y:window.innerHeight*inv,
-            w:w,h:h
+    stop(){
+        if(this.mouseup_listener){
+            console.log("cleaning up mouse up listener")
+            window.removeEventListener(this.mouseup_listener)
         }
-        this.aim_rect = {
-            x:window.innerWidth*inv,
-            y:window.innerHeight*inv,
-            w:w,h:h
-        }
-    }
-
-    get_pad_vector(touch,rect){
-        const mid = {x:rect.x + rect.w/2, y: rect.y + rect.h/2}
-        return {x: mid.x - touch.clientX, y: mid.y - touch.clientY}
-    }
-
-    update_touch_dpad(touch){
-        // convert to -1 to 1 in the 20% corner of the window
-        const v = this.get_pad_vector(touch,this.dpad_rect)
-        console.log("dpad",v,{x:touch.clientX,y:touch.clientY},this.dpad_rect)
-        this.direction = v
-    }
-
-    update_touch_aim(touch){
-        const v = this.get_pad_vector(touch,this.aim_rect)
-        console.log("aim",v,{x:touch.clientX,y:touch.clientY},this.aim_rect)
-        this.mouse.x = v.x; 
-        this.mouse.y = v.y
     }
 
     update_direction_from_keys(){
